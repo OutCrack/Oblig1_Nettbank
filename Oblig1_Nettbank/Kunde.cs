f82fd1812a40b9a9873e5a0e8b949e21ce7ad52a
OutCrack <andreas@bjornrud.com>
2016-10-03 11:39:26 +0200

    Kølle

---
+++ Oblig1_Nettbank/Kunde.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Oblig1_Nettbank
+{
+    public class Kunde
+    {
+        //johoooooooo
+    }
+}
